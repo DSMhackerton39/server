5b1d6fad1f261a73c26153eb2743bead9f56d343
KimSeungJin <klli1naver@gmail.com>
2023-07-05 09:46:38 +0900

    update

--- core/model/models.py
+++ core/model/models.py
@@ -12,13 +12,13 @@ class TblUser(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     u_id = Column(String(255), nullable=False)
     password = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=True)
 
 
 class TblStatistic(Base):
     __tablename__ = 'tbl_statistics'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=True)
     date = Column(DATE, nullable=False, default=datetime.date.today())
     count = Column(Integer, nullable=False, default=0)
     user_id = Column(ForeignKey('tbl_user.id'), nullable=False, index=True)
